d58a24763e18fb669f9212048b95931b0d9e07cf
Eugen <eugen252009@gmail.com>
2023-10-31 20:40:16 +0100

    added express and edited fastify

---
+++ express/ReadMe.md
@@ -0,0 +1,10 @@
+# Benchmark between some languages
+
+## What is this about?
+
+This is a speed comparison between some programming languages. 
+
+### included languages:
+- GO
+- Javascript w/ Fastify
+- Javascript w/ Express 
--- results/bench.sh
+++ results/bench.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 ((GO = $(wrk -c 3000 -t 16 http://127.0.0.1:8080 | grep "requests in 10" | awk {'print $1'})))
-((JS = $(wrk -c 3000 -t 16 http://127.0.0.1:3000 | grep "requests in 10" | awk {'print $1'})))
+((express = $(wrk -c 3000 -t 16 http://127.0.0.1:3000 | grep "requests in 10" | awk {'print $1'})))
+((fastify = $(wrk -c 3001 -t 16 http://127.0.0.1:3000 | grep "requests in 10" | awk {'print $1'})))
 
-if (($GO > $JS ))
-then 
-	echo -e "Go was $(($GO/$JS)) x faster\n go: $GO\n js: $JS"
-else
-	echo -e "JS was $(($JS/$GO)) x faster\n go: $GO\n js: $GO"
-fi
+echo "Go was $(($GO/$express)) x faster then express"
+echo -e "go:\t\t\t$GO"
+echo -e "js express:\t\t$express"
+echo -e "js fastify:\t\t$fastify"
+
