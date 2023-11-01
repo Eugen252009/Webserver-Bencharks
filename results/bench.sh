9fb74fb80a96e1b870b4c5b245c9da8681ea9599
Eugen <eugen252009@gmail.com>
2023-11-01 10:02:30 +0100

    added c

--- results/bench.sh
+++ results/bench.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-((GO = $(wrk -c 3000 -t 16 http://127.0.0.1:8080 | grep "requests in 10" | awk {'print $1'})))
-((express = $(wrk -c 3000 -t 16 http://127.0.0.1:3000 | grep "requests in 10" | awk {'print $1'})))
-((fastify = $(wrk -c 3001 -t 16 http://127.0.0.1:3000 | grep "requests in 10" | awk {'print $1'})))
+((GO = $(wrk -c 160 -t 16 http://127.0.0.1:8080 | grep "requests in 10" | awk {'print $1'})))
+((express = $(wrk -c 160 -t 16 http://127.0.0.1:3000 | grep "requests in 10" | awk {'print $1'})))
+((fastify = $(wrk -c 160 -t 16 http://127.0.0.1:3000 | grep "requests in 10" | awk {'print $1'})))
+((c = $(wrk -c 160 -t 16 http://127.0.0.1:8081 | grep "requests in 10" | awk {'print $1'})))
 
-echo "Go was $(($GO/$express)) x faster then express"
-echo -e "go:\t\t\t$GO"
-echo -e "js express:\t\t$express"
-echo -e "js fastify:\t\t$fastify"
+echo -e "go:\t\t$GO"
+echo -e "js-express:\t$express"
+echo -e "js-fastify:\t$fastify"
+echo -e "c:\t\t$c"
 
